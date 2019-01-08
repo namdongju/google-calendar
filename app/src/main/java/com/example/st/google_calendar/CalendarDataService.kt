4350af3a767225983f1bc2223ff8b4459d3820ad
namdongju <dongju1014@naver.com>
2019-01-08 13:46:31 +0900

    init

---
+++ app/src/main/java/com/example/st/google_calendar/CalendarDataService.kt
@@ -0,0 +1,42 @@
+package com.example.st.google_calendar
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.http.HttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.DateTime
+import com.google.api.services.calendar.Calendar
+import com.google.api.services.calendar.model.CalendarList
+import com.google.api.services.calendar.model.Event
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+
+
+class CalendarDataService constructor(
+        httptransport : HttpTransport,
+        jacksonFactory: JacksonFactory,
+        googleAccountCredential : GoogleAccountCredential
+) : CalendarService {
+    private val calendar: Calendar = Calendar.Builder(httptransport, jacksonFactory, googleAccountCredential)
+            .setApplicationName("Google Calendar ").build()
+
+    override fun getCalendarList(): Single<CalendarList> {
+        return Single.fromCallable { calendar.CalendarList().list().execute() }
+                .subscribeOn(Schedulers.io())
+    }
+
+    override fun getEventList(calendarId: String): Single<List<Event>> {
+
+        var now = DateTime(System.currentTimeMillis())
+        return Single.fromCallable {
+            calendar.events()
+                    .list(calendarId)
+                    .setOrderBy("startTime")
+                    .setTimeMin(now)
+                    .setSingleEvents(true)
+                    .execute() }
+                .subscribeOn(Schedulers.io())
+                .map { it.items }
+    }
+}
+
+
